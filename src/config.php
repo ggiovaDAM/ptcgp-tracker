efb72586f8e8d5dba08a770be721136e35f0f211
Guido Giova <guido.giovaDAM@gmail.com>
2024-12-07 20:53:59 +0100

    feat: create config file
    Created config.php to standardize the directories and URLs, so they are more consistent to call.

---
+++ src/config.php
@@ -0,0 +1,15 @@
+<?php
+
+    // DIRECTORIOS
+    define("ROOT_DIR", __DIR__);
+
+    define("BACKEND_DIR", ROOT_DIR . "/backend");
+
+    define("PDO_DIR", BACKEND_DIR . "/pdo");
+
+    define("PHP_DIR", BACKEND_DIR . "/PHP");
+
+
+
+    // URLS
+    define("ROOT_URL", "http://localhost/src/");
